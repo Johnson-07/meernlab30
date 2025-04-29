1b782bb00bf3b4542ffe3e096a478c2f929af176
Johnson-07 <johnsons@karunya.edu.in>
2025-04-29 22:59:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,3 +26,41 @@ app.post('/register', async (req, res) => {
 });
 
 app.listen(5000, () => console.log('Server running on port 5000'));
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const port = 5000
+
+const app = express()
+app.use(express.urlencoded({ extended: true })); 
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect('mongodb://127.0.0.1:27017/hotelDB')
+const db = mongoose.connection
+db.once('open',()=>{
+    console.log('dbconnected')
+})
+
+const userSchema = new mongoose.Schema({
+    name:String,
+    email:String,
+    password:String,
+})
+
+const User = mongoose.model ('User',userSchema)
+
+app.post('/register',async(req,res)=>{
+    const{ name,email,password}= req.body
+    const user = new User ({name,email,password})
+    await user.save()
+    res.send('from submitted')
+})
+
+
+app.listen(port,()=>{
+    console.log(`server connected ${port}`)
+})
